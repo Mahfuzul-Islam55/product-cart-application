bf1c0b58e70f1f9e3025b0e39c9bb2a8b4a9cb99
shimulsust999 <shimulsust999@gmail.com>
2023-03-21 15:48:41 +0600

    bill details component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/component/Cart.tsx
+++ pages/component/Cart.tsx
@@ -1,27 +1,12 @@
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
+import BillDetails from "./BillDetails";
 import ShoppingCartList from "./ShoppingCartList";
 
 const Cart = () => {
   return (
     <div>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
       <main className="py-16">
         <div className="container 2xl:px-8 px-2 mx-auto">
           <h2 className="mb-8 text-xl font-bold">Shopping Cart</h2>
@@ -29,45 +14,7 @@ const Cart = () => {
             <div className="space-y-6">
               <ShoppingCartList />
             </div>
-
-            <div>
-              <div className="billDetailsCard">
-                <h4 className="mt-2 mb-8 text-xl font-bold text-center">
-                  Bill Details
-                </h4>
-                <div className="space-y-4">
-                  {/* <!-- sub total --> */}
-                  <div className="flex items-center justify-between">
-                    <p>Sub Total</p>
-                    <p>
-                      BDT <span className="lws-subtotal">8800</span>
-                    </p>
-                  </div>
-                  {/* <!-- Discount --> */}
-                  <div className="flex items-center justify-between">
-                    <p>Discount</p>
-                    <p>
-                      BDT <span className="lws-discount">0</span>
-                    </p>
-                  </div>
-                  {/* <!-- VAT --> */}
-                  <div className="flex items-center justify-between">
-                    <p>VAT</p>
-                    <p>
-                      BDT <span className="vat">0</span>
-                    </p>
-                  </div>
-                  {/* <!-- Total --> */}
-                  <div className="flex items-center justify-between pb-4">
-                    <p className="font-bold">TOTAL</p>
-                    <p className="font-bold">
-                      BDT <span className="lws-total">8800</span>
-                    </p>
-                  </div>
-                  <button className="placeOrderbtn">place order</button>
-                </div>
-              </div>
-            </div>
+            <BillDetails />
           </div>
         </div>
       </main>
