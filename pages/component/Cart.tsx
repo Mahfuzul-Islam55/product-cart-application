cfec2c2aeb33adce816bebab1cda3e0b94b4498a
shimulsust999 <shimulsust999@gmail.com>
2023-03-21 15:41:58 +0600

    shopping cart list item component

--- pages/component/Cart.tsx
+++ pages/component/Cart.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
+import ShoppingCartList from "./ShoppingCartList";
 
 const Cart = () => {
   return (
@@ -26,53 +27,9 @@ const Cart = () => {
           <h2 className="mb-8 text-xl font-bold">Shopping Cart</h2>
           <div className="cartListContainer">
             <div className="space-y-6">
-              {/* <!-- Cart Item --> */}
-              <div className="cartCard">
-                <div className="flex items-center col-span-6 space-x-6">
-                  {/* <!-- cart image --> */}
-                  <img
-                    className="lws-cartImage"
-                    src="https://i.dummyjson.com/data/products/40/thumbnail.jpg"
-                    alt="product"
-                  />
-                  {/* <!-- cart item info --> */}
-                  <div className="space-y-2">
-                    <h4 className="lws-cartName">
-                      Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptop
-                    </h4>
-                    <p className="lws-cartCategory">Men's clothing</p>
-                    <p>
-                      BDT <span className="lws-cartPrice">1100</span>
-                    </p>
-                  </div>
-                </div>
-                <div className="flex items-center justify-center col-span-4 mt-4 space-x-8 md:mt-0">
-                  {/* <!-- amount buttons --> */}
-                  <div className="flex items-center space-x-4">
-                    <button className="lws-incrementQuantity">
-                      <i className="text-lg fa-solid fa-plus"></i>
-                    </button>
-                    <span className="lws-cartQuantity">2</span>
-                    <button className="lws-decrementQuantity">
-                      <i className="text-lg fa-solid fa-minus"></i>
-                    </button>
-                  </div>
-                  {/* <!-- price --> */}
-                  <p className="text-lg font-bold">
-                    BDT <span className="lws-calculatedPrice">2200</span>
-                  </p>
-                </div>
-                {/* <!-- delete button --> */}
-                <div className="flex items-center justify-center col-span-2 mt-4 md:justify-end md:mt-0">
-                  <button className="lws-removeFromCart">
-                    <i className="text-lg text-red-400 fa-solid fa-trash"></i>
-                  </button>
-                </div>
-              </div>
-              {/* <!-- Cart Items Ends --> */}
+              <ShoppingCartList />
             </div>
 
-            {/* <!-- Bill Details --> */}
             <div>
               <div className="billDetailsCard">
                 <h4 className="mt-2 mb-8 text-xl font-bold text-center">
---
+++ pages/component/ShoppingCartList.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import ShoppingCart from "./ShoppingCart";
+
+const ShoppingCartList = () => {
+  return (
+    <Fragment>
+      <ShoppingCart />
+    </Fragment>
+  );
+};
+
+export default ShoppingCartList;
